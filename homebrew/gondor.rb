ad1f140242edc077029db90c88e9d39e9fd0f078
Brian Rosner <brian@brosner.com>
2015-09-08 16:09:36 -0500

    added homebrew recipe

---
+++ homebrew/gondor.rb
@@ -0,0 +1,19 @@
+class Gondor < Formula
+  desc "Gondor CLI"
+  homepage "https://next.gondor.io"
+  head "https://github.com/eldarion-gondor/cli.git"
+  url "https://github.com/eldarion-gondor/cli/archive/v0.2.3.tar.gz"
+  sha256 "71923e6afc23583e1a8123f2272bd88e4d32e0c6043b77c8dcf27655e353e07d"
+
+  depends_on "go" => :build
+
+  def install
+    ENV["VERSION"] = "0.2.3"
+    system "make", "homebrew"
+    bin.install "bin/gondor"
+  end
+
+  test do
+    system "#{bin}/gondor", "--version"
+  end
+end
