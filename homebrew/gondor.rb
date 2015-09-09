6ad87d5591347c8394b6f024b9f80b8725451c1f
Brian Rosner <brian@brosner.com>
2015-09-08 21:29:46 -0500

    updated homebrew for v0.3.3

--- homebrew/gondor.rb
+++ homebrew/gondor.rb
@@ -2,8 +2,8 @@ class Gondor < Formula
   desc "Gondor CLI"
   homepage "https://next.gondor.io"
   head "https://github.com/eldarion-gondor/cli.git"
-  url "https://github.com/eldarion-gondor/cli/archive/v0.3.2.tar.gz"
-  sha256 "a77e29a3cf734c3f84225a908bfe8a83003976bf32352dd53f2a1ff00515f333"
+  url "https://github.com/eldarion-gondor/cli/archive/v0.3.3.tar.gz"
+  sha256 "3091bdb8e3479274eec113dc5cedf60a063b5dfa9c189116e21fba3b903feb3f"
 
   depends_on "go" => :build
 
